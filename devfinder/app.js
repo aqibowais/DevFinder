211be8bdd7fe56e5d1a3512d88699caee7800e4b
Aqib Owais <owaisharoon60@gmail.com>
2024-08-05 13:18:47 +0500

    responsive

--- devfinder/app.js
+++ devfinder/app.js
@@ -1,15 +1,13 @@
 const baseUrl = "https://api.github.com/users/";
 const defaultUser = "aqibowais";
 
-const toggleButton = document.querySelector(".toggle");
-
 document.addEventListener("DOMContentLoaded", () => {
   getUserDetail(defaultUser);
 
-  const savedMode = localStorage.getItem('mode')
-  if(savedMode){
-    document.body.classList.add(savedMode)
-  }
+  
+
+  const toggleButton = document.querySelector(".toggle");
+  toggleButton.addEventListener('click', toggleMode);
 });
 
 function getUserDetail(username) {
@@ -24,59 +22,48 @@ function getUserDetail(username) {
       return response.json();
     })
     .then((data) => {
+      document.querySelector("#user-input").value = ""
       userDetails.innerHTML = `
-                <div class="user-img">
-                    <img src="${data.avatar_url}" alt="User Avatar">
-                </div>
-                <div class="data">
-                    <div class="uname">
-                        <div class="name">
-                            ${data.login}
-                            <br>
-                            <a href="${data.html_url}">${
-        data.name || "No name provided"
-      }</a>
-                        </div>
-                        <div class="date">
-                            <h3>Joined ${new Date(
-                              data.created_at
-                            ).toLocaleDateString()}</h3>
-                        </div>
-                    </div>
-                    <div class="repos">
-                        <ul>Repos <br> ${data.public_repos}</ul>
-                        <ul>Followers <br> ${data.followers}</ul>
-                        <ul>Following <br> ${data.following}</ul>
-                    </div>
-                    <div class="links">
-                        <ul><i class="fa-solid fa-location-dot"></i> ${
-                          data.location || "No location"
-                        } <br><br> <i class="fa-brands fa-twitter"></i> ${
-        data.twitter_username || "No Twitter"
-      }</ul>
-                        <ul><i class="fa-solid fa-link"></i> <a href="${
-                          data.html_url
-                        }">${
-        data.html_url
-      }</a> <br><br><i class="fa-solid fa-building"></i> <a href="${
-        data.html_url
-      }">Github</a></ul>
-                    </div>
-                </div>
-            `;
+        <div class="user-img">
+          <img src="${data.avatar_url}" alt="User Avatar">
+        </div>
+        <div class="data">
+          <div class="uname">
+            <div class="name">
+              ${data.login}
+              <br>
+              <a href="${data.html_url}">${data.name || "No name provided"}</a>
+            </div>
+            <div class="date">
+              <h3>Joined ${new Date(data.created_at).toLocaleDateString()}</h3>
+            </div>
+          </div>
+          <div class="repos">
+            <ul>Repos <br> ${data.public_repos}</ul>
+            <ul>Followers <br> ${data.followers}</ul>
+            <ul>Following <br> ${data.following}</ul>
+          </div>
+          <div class="links">
+            <ul><i class="fa-solid fa-location-dot"></i> ${data.location || "No location"} <br><br> <i class="fa-brands fa-twitter"></i> ${data.twitter_username || "No Twitter"}</ul>
+            <ul><i class="fa-solid fa-link"></i> <a href="${data.html_url}">${data.html_url}</a> <br><br><i class="fa-solid fa-building"></i> <a href="${data.html_url}">Github</a></ul>
+          </div>
+        </div>
+      `;
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
       document.querySelector(".user-details").innerHTML = `
-                <p>No user found. Please try another username.</p>
-            `;
+        <p>No user found. Please try another username.</p>
+      `;
     });
 }
+
 function toggleMode() {
-  const isDarkMode = document.body.classList.toggle("dark-mode");
-  const newMode = isDarkMode ? "dark-mode" : "light-mode";
-  localStorage.setItem('mode',newMode);
-  toggleButton.innerHTML = isDarkMode
-    ? 'Light <span><i class="fas fa-sun"></i></span>'
-    : 'Dark <span><i class="fas fa-moon"></i></span>';
+  const isLightMode = document.body.classList.toggle("light-mode");
+  const newMode = isLightMode ? "light-mode": "dark-mode";
+  localStorage.setItem('mode', newMode);
+  const toggleButton = document.querySelector(".toggle");
+  toggleButton.innerHTML = isLightMode
+  ? 'Dark <span><i class="fas fa-moon"></i></span>'
+  : 'Light <span><i class="fas fa-sun"></i></span>'
 }
